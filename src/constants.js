dea378eecc15c69c62ce57696fc283a7917cdb64
arnavvjainn <jain.arnavv@gmail.com>
2024-05-03 22:18:36 -0400

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. This is where I grinded during my previous internships, this is sort of a summary of them`,
+  "cs-degree": `This is where I'll hopefully hang my CS degree. I'm currently a final year student at the University of Waterloo :)`,
+  "sofa-table": `That's my sofa. This is where I relax.`,
+  tv: `That's my TV. If you have any Movie or TV show recommendations, let me know!!!!`,
+  bed: `Oh you found my bed, nothing to see here`,
+  resume: `Oh looks like you found my resume lying on my desk 😉. <a href="https://arnavvjainn.github.io/Resume/" target="_blank">Have a look at here!</a>
+  Contact me at a245jain@uwaterloo.ca if you have any interesting job opportunities!`,
+  projects: `Other projects i have done are: <br>
+  - <a href="https://github.com/arnavvjainn/SPshORTS" target="_blank">SPshORTS</a>:A full-stack sports news app which webscrapes sports articles from various news sites and summarizes them into a 60-word article using text summarization tool of the CohereAI API<br>
+  - <a href="https://github.com/rishadluthra/cs446-project/tree/main" target="_blank">Beacon</a>:Collaborated to build an android app that connects people looking for help with basic tasks with those within physical proximity able to help with those tasks<br>
+  - <a href="https://github.com/arnavvjainn/FootballData" target="_blank">FootballData</a>:A data visualization tool which analyzes football data of various footballing competitions to understand patterns of play and establish relationship between different scenarios<br> 
+  why not have a look at my <a href="https://arnavvjainn.github.io/Resume/" target="_blank">Resume</a>  <----------`,
+  library: `I am currently reading Designing Data Intesive Applications. I am really into start-ups and how to build one, i got great insight by reading "Fall in Love with the Problem, Not the Solution by Uri Lavine and am always open to suggestions`,
+  exit: `If you want to exit Arnav Jain's portfolio, just close the tab.`,
+};
+
+dialogueData.pc += `<ul>
+<li><h2>Tata Consultancy Services as a Software Developer Intern</h2></li>
+<li><h2>Ontario Lottery and Gaming as a Technology Security Analyst Intern</h2></li>
+<li><h2>The Globe and Mail as a Software Engineering Intern</h2></li>
+<li><h2>Loblaws Companies Limitied as a Software Engineering Intern</h2></li>
+</ul>
+why not have a look at my <a href="https://arnavvjainn.github.io/Resume/" target="_blank">Resume</a>  <----------`
